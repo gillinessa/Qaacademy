3e991639967424c269a1bbaa1ba1c0fe70ddcea2
gillinessa <gilli_inacio@hotmail.com>
2023-04-15 15:03:24 -0300

    exercico metodos

---
+++ cursoAltomacao/src/Exer11.java
@@ -0,0 +1,35 @@
+        public class Exer11 { 
+            // CRIAR 1 METODO com retorno que monte uma frase com os valores que recebeu por  parametro
+
+            public static String texto (String primeirotexto, String segundotexto){
+                String resultado = primeirotexto +segundotexto;
+                return resultado;
+            }
+
+            public static void main(String[]args){
+                String primeirotexto = "primeiro texto leitura";
+                String segundotexto = "segundo texto concatena";
+                System.out.println(texto(primeirotexto, segundotexto));
+
+                int nume2=
+            }
+            // criar 1metodos com retorno  que some dois valores e se for maior que 100, retorne true. caso contrario, retorne else 
+            public static boolean maiorquecem(int valor1, int valor2){
+                int resultado = valor1+valor2;
+                if(resultado>100){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+                
+                
+            
+
+        
+
+
+
+
+
+
---
+++ cursoAltomacao/src/Exerci10.java
@@ -0,0 +1,26 @@
+public class Exerci10 {
+    public static void main (String[] args){
+    System.out.println(multiplicacao());
+    adicao();
+
+    
+
+    }
+
+    public static void adicao (){
+
+         int nu1 = 40;
+         int nu2 = 20;
+
+         System.out.println(nu1+nu2);
+
+    }
+    public static int  multiplicacao(){
+        int mult1 = 4;
+        int mult2 = 2;
+        int resultado = mult1*mult2;
+        return resultado;
+
+    }
+    
+}
